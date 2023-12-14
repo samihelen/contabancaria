2c0690ccbd50a05ad58f706fdc3aebf093b84064
helensamira <samira.doc24@gmail.com>
2023-12-14 15:22:06 -0300

    Inseri pacote conta.model e a Classe Conta, realizei alguns teste

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,42 @@
 package conta;
 
+import java.util.Scanner;
 import conta.util.Cores;
 
 public class Menu {
 	
 	public static void main(String[] args) {
-		System.out.println(Cores.class);
+		
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		
+		while (true) {
+			
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					          +"*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                  BANCO DO BRAZIL                    ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     "+Cores.TEXT_RESET);
+			
+			opcao = leia.nextInt();
+
+		}
 	}
+	
+	
 }

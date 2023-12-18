a919ab648adf66172c3eaeb3fed4537bfbbd47d5
helensamira <samira.doc24@gmail.com>
2023-12-18 17:11:36 -0300

    Feitos testes das contas corrente e poupança e criação dos métodos

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,8 +1,9 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.contaCorrente;
-import conta.model.contaPoupanca;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -12,6 +13,29 @@ public class Menu {
 		Scanner leia = new Scanner(System.in);
 		int opcao;
 		
+		//teste classe conta
+				Conta c1 = new Conta(1, 123, 1, "Helen", 10000.0f);
+				c1.visualizar();
+				c1.sacar(12000.0f);
+				c1.visualizar();
+				c1.depositar(5000.0f);
+				c1.visualizar();
+				
+		// Teste da Classe Conta Corrente
+				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "Gustavo Santos", 0.0f, 1000.0f);
+				cc1.visualizar();
+				cc1.sacar(12000.0f);
+				cc1.visualizar();
+				cc1.depositar(5000.0f);
+				cc1.visualizar();
+				
+		// Teste da Classe Conta Poupança
+				ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Rebeca Santos", 100000.0f, 15);
+				cp1.visualizar();
+		        cp1.sacar(1000.0f);
+				cp1.visualizar();
+				cp1.depositar(5000.0f);
+				cp1.visualizar();
 		while (true) {
 			
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
@@ -36,9 +60,59 @@ public class Menu {
 			System.out.println("                                                     "+Cores.TEXT_RESET);
 			
 			opcao = leia.nextInt();
-
+			
+			if (opcao == 9) {
+				System.out.println("\nBanco do Brazil - O seu futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			} switch (opcao) {
+			case 1:
+				System.out.println("Criar Conta\n\n");
+				
+				break;
+			case 2:
+				System.out.println("Listar todas as contas\n\n");
+				
+				break;
+			case 3:
+				System.out.println("Consultar dados da Conta - por número\n\n");
+				
+				break;
+			case 4:
+				System.out.println("Atualizar dados da Conta\n\n");
+				
+				break;
+			case 5:
+				System.out.println("Apagar a Conta\n\n");
+				
+				break;
+			case 6:
+				System.out.println("Saque\n\n");
+				
+				break;
+				
+			case 7:
+				System.out.println("Depósito\n\n");
+				
+				break;
+				
+			case 8:
+				System.out.println("Transferência entre Contas\n\n");
+				
+				break;
+			default:
+				System.out.println("\nOpção Inválida\n");
+				break;
+			
+			}
 		}
 	}
-	
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Helen Samira ");		
+		System.out.println("https://github.com/samihelen");
+		System.out.println("*********************************************************");
+	}
 	
 }
--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -57,5 +57,45 @@ public class Conta {
 		this.saldo = saldo;
 	}
 	
+	public boolean sacar(float valor) {
+		
+		if(this.getSaldo()< valor) {
+			System.out.println("\nSaldo Insuficiente");
+			return false;
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+	}
 	
-}
+	public void depositar(float valor) {
+		
+		this.setSaldo(this.getSaldo() + valor);
+	}
+	
+	public void visualizar() {
+		String tipo = "";
+		
+		switch(this.tipo) {
+		case 1:
+				tipo = "Conta Corrente";
+		break;
+		case 2:
+				tipo = "Conta Poupança";
+		break;
+		}
+		
+		System.out.println("\n\n***********************************************************");
+		System.out.println("Dados da Conta:");
+		System.out.println("***********************************************************");
+		System.out.println("Numero da Conta: " + this.numero);
+		System.out.println("Agência: " + this.agencia);
+		System.out.println("Tipo da Conta: " + tipo);
+		System.out.println("Titular: " + this.titular);
+		System.out.println("Saldo: " + this.saldo);
+	}
+
+	
+		
+	}
+
